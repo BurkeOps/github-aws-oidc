476762f1ad7cfec38586f5f52be8af1df87122ef
Danny Burke <dburke9607@gmail.com>
2022-07-15 14:32:12 +0100

    init commit

---
+++ lib/provider.ts
@@ -0,0 +1,29 @@
+import {Stack, StackProps}from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import {CfnOIDCProvider} from 'aws-cdk-lib/aws-iam';
+
+export interface OIDCConnectorProps extends StackProps {
+  repositoryName: string;
+  envName: string;
+}
+
+export class OIDCConnector extends Stack {
+  static THUMB_PRINT_ID = '6938FD4D98BAB03FAADB97B34396831E3780AEA1';
+  static OIDC_URL = 'https://token.actions.githubusercontent.com';
+  static STS_URL = 'sts.amazonaws.com';
+
+  constructor(
+    scope: Construct,
+    id: string,
+    props: OIDCConnectorProps
+  ) {
+    super(scope, id, props);
+
+
+    new CfnOIDCProvider(this, 'OIDCConnection', {
+      url: OIDCConnector.OIDC_URL,
+      clientIdList: [OIDCConnector.STS_URL],
+      thumbprintList: [OIDCConnector.THUMB_PRINT_ID],
+    });
+  }
+}
